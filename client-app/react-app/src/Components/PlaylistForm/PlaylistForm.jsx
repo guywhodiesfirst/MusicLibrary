88ad13bc69b27f3c4dcf25412812137ac4138423
guywhodiesfirst <brizeout@gmail.com>
2024-12-20 03:26:54 +0200

    Add client-side playlist creating

---
+++ client-app/react-app/src/Components/PlaylistForm/PlaylistForm.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import "./PlaylistForm.css";
+
+export default function PlaylistForm({ onSubmit }) {
+  const [description, setDescription] = useState("");
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = () => {
+    if (description.length > 400) {
+      setError("Description cannot exceed 400 characters.");
+      return;
+    }
+    if(name === "") {
+        setError("Playlist name should be filled")
+        return;
+    }
+
+    if (name.length > 80) {
+        setError("Playlist name cannot exceed 80 characters.");
+        return;
+    }
+    const playlistData = { name, description };
+    onSubmit(playlistData);
+    setError("");
+    setDescription("");
+    setName("");
+  };
+
+  const handleDescriptionChange = (e) => {
+    const value = e.target.value;
+    if (value.length <= 400) {
+      setDescription(value);
+      setError("");
+    } else {
+      setError("Description cannot exceed 400 characters.");
+    }
+  };
+
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    if (value.length <= 80) {
+      setName(value);
+      setError("");
+    } else {
+      setError("Playlist name cannot exceed 80 characters.");
+    }
+  }
+
+  return (
+    <div className="playlist-form">
+      <h2>Create Playlist</h2>
+      <div className="playlist-form-data">
+        <input
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Enter playlist name"
+            className="playlist-name-input"
+        />
+        <p>{name.length} / 80</p>
+        <textarea
+            value={description}
+            onChange={handleDescriptionChange}
+            placeholder="Write description here (optional)..."
+            className="playlist-description-input"
+            rows="10"
+        ></textarea>
+        <p>{description.length} / 400</p>
+        {error && <p className="error">{error}</p>}
+      </div>
+      <br />
+      <button onClick={handleSubmit}>Create</button>
+    </div>
+  );
+}
--- client-app/react-app/src/Pages/PlaylistCreatePage/PlaylistCreatePage.jsx
+++ client-app/react-app/src/Pages/PlaylistCreatePage/PlaylistCreatePage.jsx
@@ -1,13 +1,53 @@
 import { useContext, useState, useEffect } from "react";
 import { Context } from "../../App";
+import { useNavigate } from "react-router-dom";
 import './PlaylistCreatePage.css'
 import { PlaylistApi } from "../../API/PlaylistsApi";
+import PlaylistForm from "../../Components/PlaylistForm/PlaylistForm";
 
-export default function ProfilePage() {
-    
+export default function PlaylistCreatePage() {
+    const { isAuthenticated, user } = useContext(Context)
+    const [profile, setProfile] = useState(null)
+    const [loading, setIsLoading] = useState(false)
+    const [error, setError] = useState('')
+    const [isCurrentUserPage, setIsCurrentUserPage] = useState(false)
+    const [playlists, setPlaylists] = useState([])
+    const navigate = useNavigate()
+
+    const handlePlaylistCreate = async (playlist) => {
+        try {
+            setIsLoading(true)
+            const response = await PlaylistApi.createPlaylist(user.id, playlist);
+            if (response.success) {
+                navigate('/me')
+            } else {
+                alert(response.message);
+            }
+        } catch {
+            alert("Unexpected error while trying to create playlist. Try again.");
+        } finally {
+            setIsLoading(false)
+        }
+    }
+    useEffect(() => {
+        if(!isAuthenticated) {
+            navigate('/login')
+        }
+    }, []);
+
+    if(loading) {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
     return(
-        <div>
-            hi :D
+        <div className="playlist-create-page">
+            <div className="playlist-create-wrapper">
+                <PlaylistForm onSubmit={handlePlaylistCreate}/>
+            </div>
         </div>
     )
 }
